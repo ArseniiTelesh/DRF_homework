eab6629575b8a7338931c142b7a22a83e8ebf1bf
ArseniiTelesh <telesh.sen98@gmail.com>
2025-05-03 01:25:43 +0400

    made 3 task

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
@@ -12,10 +20,3 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
