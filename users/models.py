aae953bf6ce963553eb05f6f1258b458d54c0077
ArseniiTelesh <telesh.sen98@gmail.com>
2025-05-03 01:11:54 +0400

    made 2 task

--- users/models.py
+++ users/models.py
@@ -1,8 +1,13 @@
+from tkinter.constants import CASCADE
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from materials.models import Course, Lesson
+
 NULLABLE = {"blank": True, "null": True}
+
 
 class User(AbstractUser):
     username = None
@@ -17,10 +22,31 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-
     def __str__(self):
         return f"{self.email}"
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_date = models.DateField(auto_now=True, verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Оплаченный курс', **NULLABLE)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='Оплаченный урок', **NULLABLE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f"{self.user} - {self.amount} ({self.payment_date})"
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ['-payment_date']
